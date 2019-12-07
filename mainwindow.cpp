b65e87a24f03fc4107da7283543e8ac1d9ca0de9
Alex <tseplyaeff.alex@yandex.ru>
2019-12-07 13:51:40 +0900

    v0.3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,89 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::addLabel(QPushButton *button, QLabel *label) {
+    double all_numbers; // переменная для записи всех чисел
+
+    if (label->text() == "0")
+        label->setText(button->text());
+    else {
+        all_numbers = (ui->display->text() + button->text()).toDouble();
+        label->setText(QString::number(all_numbers, 'g', 10));
+    }
+}
+
+void MainWindow::on_pushButton_1_clicked()
+{
+    addLabel(ui->pushButton_1, ui->display);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    addLabel(ui->pushButton_2, ui->display);
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    addLabel(ui->pushButton_3, ui->display);
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    addLabel(ui->pushButton_4, ui->display);
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    addLabel(ui->pushButton_5, ui->display);
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    addLabel(ui->pushButton_6, ui->display);
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    addLabel(ui->pushButton_7, ui->display);
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    addLabel(ui->pushButton_8, ui->display);
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    addLabel(ui->pushButton_9, ui->display);
+}
+
+void MainWindow::on_pushButton_0_clicked()
+{
+    addLabel(ui->pushButton_0, ui->display);
+}
+
+void MainWindow::on_CE_clicked()
+{
+    if (num.getValue() != 0)
+        num.setValue(0);
+    ui->display->setText("0");
+}
+
+void MainWindow::on_erase_clicked()
+{
+    QString str;
+    str = ui->display->text();
+    unsigned size = str.size();
+    str.resize(size-1);
+    if (str == "") str = "0";
+    ui->display->setText(str);
+}
+
+void MainWindow::on_pushButton_dot_clicked()
+{
+    // если в строке нет символа точки, то добавляем его
+    if (!(ui->display->text().contains('.'))) {
+        ui->display->setText(ui->display->text() + ".");
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "calculator.h"
+#include <QPushButton>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,8 +18,39 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_1_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_0_clicked();
+
+    void on_CE_clicked();
+
+    void on_erase_clicked();
+
+    void on_pushButton_dot_clicked();
+
 private:
     Ui::MainWindow *ui;
+    // переменная для вычислений
+    Calculator num;
+    // функция добавления текста на экран
+    void addLabel(QPushButton *button,QLabel *label);
 
 };
 #endif // MAINWINDOW_H
